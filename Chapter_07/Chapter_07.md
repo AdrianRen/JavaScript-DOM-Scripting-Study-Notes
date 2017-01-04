f287264d341c0ff4f48bd1f483a64bd3d18c20ae
Adrian Ren <adrian.ren@outlook.com>
2017-01-04 22:03:39 +0800

    Updating

---
+++ Chapter_07/Chapter_07.md
@@ -0,0 +1,4 @@
+#Chapter 7 动态创建标记
+
+##1. document.write
+- document.write 最大的缺点就是违背了"行为应该和表现分离"
---
+++ Chapter_07/scripts/exampl.js
@@ -0,0 +1,8 @@
+function insertParagraph(text) {
+    var str = "<p>";
+    str += text;
+    str += "</p>";
+    document.write(str);
+}
+
+insertParagraph("All those moments will be lost in time!")
