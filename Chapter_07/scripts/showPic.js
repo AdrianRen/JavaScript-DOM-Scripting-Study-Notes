08a380b4348e14935e22e5c8a720cec55f904b49
Adrian Ren <adrian.ren@outlook.com>
2017-01-09 21:15:07 +0800

    update

--- Chapter_07/scripts/showPic.js
+++ Chapter_07/scripts/showPic.js
@@ -139,7 +139,7 @@ function prepareGallery() {
     let links = gallery.getElementsByTagName("a");
     for (var i = 0; i < links.length; i++) {
       links[i].onclick = function () {
-          return !showPic(this);
+          return showPic(this);
       }
       links[i].onkeypress = links[i].onclick;
     }
