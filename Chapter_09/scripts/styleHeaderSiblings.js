be38480f136265c049818001a3e5e5011825e91b
Adrian Ren <adrian.ren@outlook.com>
2017-01-16 22:31:54 +0800

    Update

---
+++ Chapter_09/scripts/styleHeaderSiblings.js
@@ -0,0 +1,15 @@
+function styleHeaderSiblings() {
+  if (!document.getElementByTagName) return false;
+  let headers = document.getElementByTagName("h1");
+  let elem;
+  for (var i = 0; i < headers.length; i++) {
+    elem = getNextElement(headers[i].nextSibling);
+    elem.style.fontWeight = "bold";
+    elem.style.fontSize = "1.2em";
+  }
+}
+function getNextElement() {
+  if (node.nodeType == 1) {
+
+  }
+}
