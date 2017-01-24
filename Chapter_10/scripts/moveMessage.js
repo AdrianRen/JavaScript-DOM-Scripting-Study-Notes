e7b8f85f68b8f22dbf2eb01f2852f0ffeb519a2f
Adrian Ren <adrian.ren@outlook.com>
2017-01-24 18:09:22 +0800

    updates

--- Chapter_10/scripts/moveMessage.js
+++ Chapter_10/scripts/moveMessage.js
@@ -1,5 +1,23 @@
 function moveMessage() {
   let elem = document.getElementById('msg');
-  elem.style.left = "400px";
+  //Use parseInt method to trans string to integer
+  let xpos = parseInt(elem.style.left);
+  let ypos = parseInt(elem.style.top);
+  if (xpos === 200 && ypos === 100) {
+    return true;
+  }
+  if (xpos>200) {
+    xpos--;
+  }
+  if (ypos<100) {
+    ypos++;
+  }
+  if (ypos>100) {
+    ypos--;
+  }
+  // elem.style.left = "400px";
+  elem.style.left = `${xpos}px`;
+  elem.style.top = `${ypos}px`;
+  movement = setTimeout("moveMessage()",10);
 }
-addLoadEvent(moveMessage);
+// addLoadEvent(moveMessage);
