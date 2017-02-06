2bddff5ad63c5be9c493762bee529df17a2e9b4f
Adrian Ren <adrian.ren@outlook.com>
2017-02-06 17:23:23 +0800

    updates

--- Chapter_12/scripts/global.js
+++ Chapter_12/scripts/global.js
@@ -54,3 +54,30 @@ function highlightPage() {
     }
 }
 addLoadEvent(highlightPage);
+
+function moveElement(elementID,final_x,final_y,interval) {
+  if (!document.getElementById) return false;
+  if (!document.getElementById(elementID)) return false;
+  var elem = document.getElementById(elementID);
+  let xpos = parseInt(elem.style.left);
+  let ypos = parseInt(elem.style.top);
+  if (xpos == final_x && ypos == final_y) {
+    return true;
+  }
+  if (xpos < final_x) {
+    xpos++;
+  }
+  if (xpos > final_x) {
+    xpos--;
+  }
+  if (ypos < final_y) {
+    ypos++;
+  }
+  if (ypos > final_y) {
+    ypos--;
+  }
+  elem.style.left = xpos + "px";
+  elem.style.top = ypos + "px";
+  let repeat = "moveElement('"+elementID+"',"+final_x+","+final_y+","+interval+")"
+  movement = setTimeout(repeat,interval);
+}
